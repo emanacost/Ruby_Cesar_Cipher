9fa92fb4734e1ad5f9c8e2f60c68140609e94775
eacosta <eacosta0411@gmail.com>
2025-09-26 13:52:16 -0400

    Add overall plan

--- Cesar Cipher.rb
+++ Cesar Cipher.rb
@@ -1,3 +1,18 @@
+# Overall Plan
+    # Take input from console for a string and a shift
+    # Put input into an array
+    # Split the array into letters
+    # Convert letters into ascii code
+    # Shift the letters
+    # Convert back to letters
+    # Put the shifted letters into an array
+    # Combine the shifted letters back into words
+    # Return the shifted array as an out put in the console
+
+
+
+
+
 # Method to shift capital letters
 def cap_shifter(letter, shift)
     ascii_letter = letter.ord
