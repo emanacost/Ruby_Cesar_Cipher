912ba2b14b332ae51ca90ce09d7982cf4d76994b
eacosta <eacosta0411@gmail.com>
2025-09-26 20:40:37 -0400

    Add input from console with handling

--- Cesar Cipher.rb
+++ Cesar Cipher.rb
@@ -10,7 +10,16 @@
     # Return the shifted array as an out put in the console
 
 
-
+# Take input from console
+unshift_string = ARGV[0].to_s()
+shift_key = ARGV[1].to_i()
+# Check if input is valid
+if unshift_string.is_a?(String) && shift_key != 0
+    puts "Your unencrypted message is: #{unshift_string}"
+    puts "Your message will shift by #{shift_key}"
+else
+    abort('Error! Please input as follows: Cesar Cipher.rb "Message" 2')
+end
 
 
 # Method to shift capital letters
