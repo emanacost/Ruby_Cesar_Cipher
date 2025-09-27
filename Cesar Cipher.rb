f94285ccb5bfa4e8f22b28cac5ea5373f0589ab3
eacosta <eacosta0411@gmail.com>
2025-09-26 21:07:56 -0400

    Add line to split the string into an array of the individual letters

--- Cesar Cipher.rb
+++ Cesar Cipher.rb
@@ -21,6 +21,8 @@ else
     abort('Error! Please input as follows: Cesar Cipher.rb "Message" 2')
 end
 
+# Split unshift_string into an array of individual letters
+unshift_string_split = unshift_string.split("")
 
 # Method to shift capital letters
 def cap_shifter(letter, shift)
